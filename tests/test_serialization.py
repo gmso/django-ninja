5fd2e35d1604dfae73735aa8edc5c0ea28e231f8
gmso <german.mene@gmail.com>
2022-01-07 22:03:22 +0100

    Add failing test

--- tests/test_serialization.py
+++ tests/test_serialization.py
@@ -1,36 +1,34 @@
-import pytest
 import datetime
 
-from tests.demo_project.someapp import models
+from django.urls import reverse
+import pytest
+
+from someapp import models
 
 
 def db_factory():
-    goal = models.Goal.create(name="Learn foreign languages")
-    todo_italian = models.Todo.create(
+    goal = models.Goal.objects.create(name="Learn foreign languages")
+    todo_italian = models.Todo.objects.create(
         title="Study italian",
         completed_at = datetime.datetime(2022,10,1,1,10,20),
         goal = goal
     )
-    todo_project = models.Todo.create(
+    todo_project = models.Todo.objects.create(
         title="Work on project A",
         completed_at = datetime.datetime(2022,10,1,1,10,20),
     )
-    todo_dishes = models.Todo.create(title="Wash the dishes")
-    todo_french = models.Todo.create(
+    todo_dishes = models.Todo.objects.create(title="Wash the dishes")
+    todo_french = models.Todo.objects.create(
         title="Study french",
         goal = goal
     )
-    tag_home = models.Tag.create(
-        name = "Home",
-        todos = [todo_dishes, todo_french]
-    )
-    tag_work = models.Tag.create(
-        name = "Work",
-        todos = [todo_project]
-    )
-    ranking_1 = models.Ranking.create(position = 1, todo = todo_project)
-    ranking_2 = models.Ranking.create(position = 2, todo = todo_italian)
-    ranking_3 = models.Ranking.create(position = 3, todo = todo_french)
+    tag_home = models.Tag.objects.create(name = "Home")
+    tag_home.todos.set([todo_dishes, todo_french])
+    tag_work = models.Tag.objects.create(name = "Work")
+    tag_home.todos.set([todo_project])
+    ranking_1 = models.Ranking.objects.create(position = 1, todo = todo_project)
+    ranking_2 = models.Ranking.objects.create(position = 2, todo = todo_italian)
+    ranking_3 = models.Ranking.objects.create(position = 3, todo = todo_french)
 
     return {
         "goal": goal,
@@ -50,3 +48,15 @@ def db_factory():
             "ranking_3": ranking_3,
         }
     }
+
+
+@pytest.mark.django_db
+def test_serialize_model_FK_reversem2m_reverseone2one_queryset_no_prefetch(client):
+    """
+    Test serialization of model with Foreign Key, reverse ManyToMany and reverse
+    OneToOne relationships.
+    Test serialization of Queryset
+    NO prefetch used for reverse ManyToMany
+    """
+    objects = db_factory()
+    response = client.get("/api/events/todos/no_prefetch")
