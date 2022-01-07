64346d3d476565188d4cacd7d6187273977ce244
gmso <german.mene@gmail.com>
2022-01-07 21:28:35 +0100

    Add API endpoints for tests

--- tests/demo_project/someapp/api.py
+++ tests/demo_project/someapp/api.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel
 
 from ninja import Router
 
-from .models import Event
+from .models import Event, Todo, Tag, Goal, Ranking
 
 router = Router()
 
@@ -35,3 +35,51 @@ def list_events(request):
 def get_event(request, id: int):
     event = get_object_or_404(Event, id=id)
     return event
+
+
+@router.get("/todos/no_prefetch")
+def get_all_todos_without_prefetch(request):
+    return Todo.objects.all()
+
+
+@router.get("/todos/prefetched")
+def get_all_todos_with_prefetch(request):
+    return Todo.objects.all().prefetch_related('tag_set')
+
+
+@router.get("/tags/no_prefetch")
+def get_all_tags_without_prefetch(request):
+    return Tag.objects.all()
+
+
+@router.get("/tags/prefetched")
+def get_all_tags_with_prefetch(request):
+    return Tag.objects.all().prefetch_related('todos')
+
+
+@router.get("/goals/no_prefetch")
+def get_all_goals_without_prefetch(request):
+    return Goal.objects.all()
+
+
+@router.get("/goals/prefetched")
+def get_all_goals_with_prefetch(request):
+    return Goal.objects.all().prefetch_related('todo_set')
+
+
+@router.get("/rankings/no_prefetch")
+def get_all_rankings_without_prefetch(request):
+    return Ranking.objects.all()
+
+
+@router.get("/todos/paginated")
+def get_paginated_todos(request, page: int = 0):
+    paginate_by = 2
+    first = paginate_by * page
+    last = first + paginate_by
+    return Todo.objects.all()[first:last]
+
+
+@router.get("/todos/first")
+def get_first_todo(request):
+    return Todo.objects.get(pk=1)
